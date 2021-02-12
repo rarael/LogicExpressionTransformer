1be96e93e554e4b14768ad250de7fd89908d475f
rarael <rarael@live.unc.edu>
2021-02-12 14:04:51 -0500

    Implemented adding ands

--- src/com/transformer/Main.java
+++ src/com/transformer/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+    System.out.println("Enter logic expression: ");
     Transformer transform = new Transformer(scan);
     transform.transformLogic();
   }
--- src/com/transformer/Transformer.java
+++ src/com/transformer/Transformer.java
@@ -1,5 +1,6 @@
 package com.transformer;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Transformer {
@@ -17,22 +18,43 @@ public class Transformer {
       } else {
         StringBuilder string = new StringBuilder(currentString);
         int notIndex = string.indexOf("~");
+        int notIndex2 = string.indexOf("¬");
         int orIndex = string.indexOf("+");
         while (notIndex != -1) {
           string.replace(notIndex, notIndex + 1, "!");
           notIndex = string.indexOf("~");
         }
-        if(orIndex != -1) {
-          string.replace(orIndex,orIndex + 1, "||");
+        while (notIndex2 != -1) {
+          string.replace(notIndex2, notIndex2 + 1, "!");
+          notIndex2 = string.indexOf("¬");
         }
-        addAnds(string);
-        System.out.print(string.toString());
+        while(orIndex != -1) {
+          string.replace(orIndex,orIndex + 1, "||");
+          orIndex = string.indexOf("+");
+        }
+        String stringWithAnds = addAnds(string);
+        System.out.print(stringWithAnds);
       }
       System.out.print(" ");
     }
+    _scan.close();
   }
 
-  private void addAnds(StringBuilder string) {
-
+  private String addAnds(StringBuilder string) {
+    char prevChar = string.charAt(0);
+    ArrayList<Integer> whereToInsertAnds = new ArrayList<>();
+    int howManyInserts = 0;
+    for (int i = 1; i < string.length(); i++) {
+      char currentChar = string.charAt(i);
+      if ((Character.isLetter(prevChar) || prevChar == ')') && (Character.isLetter(currentChar) || currentChar == '!' || currentChar == '(')) {
+        whereToInsertAnds.add(i);
+      }
+      prevChar = currentChar;
+    }
+    for (int g : whereToInsertAnds) {
+      string.insert(g + (howManyInserts * 2), "&&");
+      howManyInserts++;
+    }
+    return string.toString();
   }
 }
